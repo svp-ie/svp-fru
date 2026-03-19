a4d0b19ce9214f5ac69fe62b814e793cfe5ca035
Rob Killilea <rob.killilea@svp.ie>
2026-03-19 19:08:15 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# SVP Fundraise Up Wordpress plugin
+
+This plugin allows for two accounts and their campaigns to be added on a donate page, with a mechanism to switch between them
+
+## Installation
+
+First clone this repo with `git clone`
+
+1. WIthin the Wordpress admin panel select `Plugins | Add Plugin`
+2. Now, top of the screen, click the `Upload Plugin` button
+3. Now click `Choose File` and navigate to wherever you cloned the repo and select the `svp-fru.zip` File
+4. Back at the `Add Plugins` screen, click `Install Now`
+5. Finally, when the plugin is installed click the `Activate Plugin` button
+
+## Directions for use
+
+### Fundraise Up
+
+To install Fundraise Up for account switching you need a Subaccount setup for the Giftaid (GB) option and some details from your current main account campaign
+
+1. Create a subaccount (`Settings | Subaccounts`) and within that subaccount create a campaign, or better still, clone a campaign from the main account
+
+2. Now on the Subaccounts screen you can find the ID of your subaccount. You'll also need the ID of the Campaign (`Campaigns`)
+
+3. Now switch back to the main account and again find the ID of both the account and the active campaign. The account ID is a little trickier to find on the main account, you can find it by selecting your active campaign on the Campaigns screen. The ID is top left, directly under the campaign name
+
+### Wordpress
+
+1. Add the `[fru_install]` shortcode to the site header template
+
+```
+[fru_script gb_widget="APVKVGAC" intl_widget="AVYLHELK"]
+```
+
+1. Optionally add the `[fru_button]` shortcode wherever you'd like the FRU button element to appear
+
+```
+[fru_button gb_href="#XAYBUKJT" intl_href="#XLBJGPGQ"]
+```
+
+1. Optionally add the `[fru_switch]` shortcode wherever you'd like the switch function to appear
+
+```
+[fru_switch label="Custom text"]
+```
---
+++ svp-fru/svp-fru.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Plugin Name: SVP FundraiseUp
+ * Description: Inserts the correct FundraiseUp widget based on visitor country, detected via Cloudflare.
+ * Version:     1.0.0
+ * Author:      SVP
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function svp_maybe_set_country_cookie() {
+    if ( isset( $_GET['svp_country'] ) && preg_match( '/^[A-Z]{2}$/', $_GET['svp_country'] ) ) {
+        $cc = $_GET['svp_country'];
+        setcookie( 'svp_country', $cc, [
+            'expires'  => time() + ( 60 * 60 * 24 * 90 ), // 90 days
+            'path'     => '/',
+            'secure'   => true,
+            'httponly' => false,
+            'samesite' => 'Lax',
+        ] );
+        wp_safe_redirect( remove_query_arg( 'svp_country' ) );
+        exit;
+    }
+}
+add_action( 'init', 'svp_maybe_set_country_cookie' );
+
+function svp_get_country() {
+    static $cc = null;
+    if ( $cc === null ) {
+        if ( isset( $_COOKIE['svp_country'] ) && preg_match( '/^[A-Z]{2}$/', $_COOKIE['svp_country'] ) ) {
+            $cc = $_COOKIE['svp_country'];
+        } else {
+            $raw = isset( $_SERVER['HTTP_CF_IPCOUNTRY'] ) ? $_SERVER['HTTP_CF_IPCOUNTRY'] : 'XX';
+            $cc  = preg_match( '/^[A-Z]{2}$/', $raw ) ? $raw : 'XX';
+        }
+    }
+    return $cc;
+}
+
+function svp_fru_install_shortcode( $atts ) {
+    $atts = is_array( $atts ) ? $atts : [];
+    if ( empty( $atts['gb_widget'] ) || empty( $atts['intl_widget'] ) ) {
+        return '<!-- fru_install: gb_widget and intl_widget are required -->';
+    }
+    $widget_id = ( svp_get_country() === 'GB' ) ? $atts['gb_widget'] : $atts['intl_widget'];
+    $script = <<<JS
+(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track,openCheckout'.split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o:function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a+'';t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=5;o.h=w.location.href;o.l=[];for(i=0;i<8;i++)o[l[i]]=o(l[i]);w[n]=o}})(window,document,'script','FundraiseUp','{$widget_id}');
+JS;
+    return '<script id="fru">' . $script . '</script>';
+}
+add_shortcode( 'fru_install', 'svp_fru_install_shortcode' );
+
+function svp_fru_button_shortcode( $atts ) {
+    $atts        = shortcode_atts( [ 'gb_href' => '#XAYBUKJT', 'intl_href' => '#XLBJGPGQ' ], $atts, 'fru_button' );
+    $button_href = ( svp_get_country() === 'GB' ) ? $atts['gb_href'] : $atts['intl_href'];
+    return '<a id="fru-button" href="' . esc_url( $button_href ) . '" style="display: none;"></a>';
+}
+add_shortcode( 'fru_button', 'svp_fru_button_shortcode' );
+
+function svp_fru_switch_shortcode( $atts ) {
+    $switch_cc  = ( svp_get_country() === 'GB' ) ? 'XX' : 'GB';
+    $switch_url = add_query_arg( 'svp_country', $switch_cc );
+    $defaults   = [ 'label' => ( $switch_cc === 'GB' ) ? 'Switch to UK giving' : 'Switch to international giving' ];
+    $atts       = shortcode_atts( $defaults, $atts, 'fru_switch' );
+    return '<a href="' . esc_url( $switch_url ) . '">' . esc_html( $atts['label'] ) . '</a>';
+}
+add_shortcode( 'fru_switch', 'svp_fru_switch_shortcode' );
